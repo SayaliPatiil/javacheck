c7dc62ea319e4ef8faf55f0fcb622f4edf6209ba
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2021-12-10 13:56:43 +0530

    Update systemexit.java
    AvoidLiteralsInIfCondition

--- systemexit.java
+++ systemexit.java
@@ -6,10 +6,10 @@ class Main
     public static void main(String[] args)
     {
         int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
-  
+        private static final int MAX_NUMBER_OF_REQUESTS = 5;
         for (int i = 0; i < arr.length; i++)
         {
-            if (arr[i] >= 5)
+            if (arr[i] >=  MAX_NUMBER_OF_REQUESTS)
             {
                  logger.info("exit...");
   
