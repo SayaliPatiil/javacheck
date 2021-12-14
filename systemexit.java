db556e54eba2b4dc7cfa33b16de755c4bf4a4a04
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2021-12-14 11:40:23 +0530

    Update systemexit.java

--- systemexit.java
+++ systemexit.java
@@ -5,11 +5,12 @@ class GfG
 {
     public static void main(String[] args)
     {
+      private static final int MAX_NEMBER_OF _REQUEST=5;
         int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
   
         for (int i = 0; i < arr.length; i++)
         {
-            if (arr[i] >= 5)
+            if (arr[i] >=MAX_NEMBER_OF _REQUEST )
             {
                 System.out.println("exit...");
   
