70378f85a3eacd029e85d4879f8092c12dfdf67c
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2021-12-14 14:52:58 +0530

    Update systemexit.java

--- systemexit.java
+++ systemexit.java
@@ -1,6 +1,6 @@
 package javacodechecker;
 public class RedirectWithoutExit {
-public void bad(HttpServletRequest request, HttpServletResponse response) throws Exception {
+public void bad(HttpServletRequest request, HttpServletResponse response) throws parseException {
 IO.writeLine("line before redirect");
 IO.writeLine("line before redirect");
 response.sendRedirect("/test");
