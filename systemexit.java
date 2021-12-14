7fa00d8bc58604c900e5df910d62c8261dbd5780
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2021-12-14 17:46:35 +0530

    Update systemexit.java

--- systemexit.java
+++ systemexit.java
@@ -5,6 +5,15 @@ class systemexit
 {
     public static void main(String[] args)
     {
+        try {
+          ...
+            throw IllegalArgumentException();
+            }
+         finally {
+           return r;
+         }
+
+
         public  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
         private static final int MAX_NUMBER_OF_REQUESTS = 5;
         for (int i = 0; i < arr.length; i++)
