932c862e99c584f248c8782903d0211b6cd1e551
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2021-12-14 16:01:16 +0530

    Update systemexit.java

--- systemexit.java
+++ systemexit.java
@@ -1,17 +1,14 @@
-public class Systemexit
-{
-    
-    public static void foo() {
-      try {
-        int num1 = 10/5;
-      }
-      catch (Throwable e) {
-          e.printStackTrace();
-      }
+public void notValid() throws Throwable
+    {
+       
+            final String CIPHER_INPUT = "ABCDEFG123456";
+            KeyGenerator kG = KeyGenerator.getInstance("AES");
+            SecretKey secretKey = kG.generateKey();
+            byte[] byteKey = secretKey.getEncoded();
+            SecretKeySpec secretKeySpec = new SecretKeySpec(byteKey, "AES");
+            Cipher aesCipher = Cipher.getInstance("AES");
+            aesCipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            byte[] encrypted = aesCipher.doFinal(CIPHER_INPUT.getBytes("UTF-8"));
+            IO.writeLine(IO.toHex(encrypted));
+        
     }
-
-    public static void main( String args[] ) {
-      foo();
-    
-}
-}
