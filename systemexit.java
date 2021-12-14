6a3558ddbb0a40f9c8c18d3ac64a867f669dc6ef
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2021-12-14 11:48:56 +0530

    Update systemexit.java

--- systemexit.java
+++ systemexit.java
@@ -5,7 +5,7 @@ class systemexit
 {
     public static void main(String[] args)
     {
-      private static final int MAX_NEMBER_OF _REQUEST=5;
+      private static final int MAX_NEMBER_OF_REQUEST=5;
         int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
   
         for (int i = 0; i < arr.length; i++)
