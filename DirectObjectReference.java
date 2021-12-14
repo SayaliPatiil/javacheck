e7c7eb0f01a75b5cee0536f9dc430a152db0d0c7
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2021-12-14 16:21:31 +0530

    Update DirectObjectReference.java

--- DirectObjectReference.java
+++ DirectObjectReference.java
@@ -3,11 +3,12 @@ package javacodechecker;
 
 
 public class DirectObjectReference{
+ public final static int INT_ARRAY[] = {1,2,3,4,5};
 @RequestMapping(value = "/records/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 @Timed
 public ResponseEntity < Record > get(@PathVariable Long id) {
 return log.debug("REST request to get record" + id);
- public final static int INT_ARRAY[] = {1,2,3,4,5};
+ 
 }
 
 }
